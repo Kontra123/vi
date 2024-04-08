64d75111e518ab984a9778db556e6706eb9aff8e
Kontra123 <33194011+Kontra123@users.noreply.github.com>
2024-04-08 17:35:04 +0300

    removed unused files

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-// By default each test has a default timeout of 5 seconds
-// After 5 seconds jest exits from the test
-// Here i set to 30 seconds
-console.log('Running jest.setup.js')
-jest.setTimeout(30*1000); // 30 seconds
